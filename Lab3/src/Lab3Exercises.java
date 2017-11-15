78e07eee1a6f51bf73281794d5d89593dd92b0ae
4015a8 <4015a8@jobo.ece.uprm.edu>
2017-11-15 12:41:51 -0400

    -Changed JFrame title, changed the location of the window to the center of the display, and uncommented the 2nd line of the main method

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,10 @@
 import javax.swing.JFrame;
-
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Maria Alejandra Marrero Ortiz");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocationRelativeTo(null);
 		myFrame.setSize(200, 200);
+		myFrame.setLocationRelativeTo(null);
 		myFrame.setVisible(true);
 	}
 }
