c325c5e1d66c8b92f077f177ba64001cab2c02ec
Tita19 <Tita19@10.21.47.28>
2017-11-16 18:13:11 -0400

    Created PuertoRican Flag

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -3,7 +3,7 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Maria Alejandra Marrero Ortiz");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setSize(200, 200);
+		myFrame.setSize(250, 200);
 		myFrame.setLocationRelativeTo(null);
 		myFrame.setVisible(true);
         MyPanelClass myPanel = new MyPanelClass();
